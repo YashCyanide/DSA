ad4ec7bc7cf00d21333028a4a213c22b0eb6fc18
YashCyanide <ysonavane893@gmail.com>
2025-10-29 13:45:46 +0530

    initial setup and start with big O

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+// Omega is best case
+// Theta is average case
+// omicron is worst case also called as Big O notations,
+// Even though Big O is technically the worst case but in general use used for all complexities.
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+
+}
